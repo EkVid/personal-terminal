2abb66b8ce4345018c68eb815bed85fc7f41d227
Austin <austinjas014@gmail.com>
2025-03-04 21:28:51 -0500

    attempted to fix build error

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import UnixTerminal from "./components/UnixTerminal";
+'use client';
+import dynamic from "next/dynamic";
+
+
+const TerminalComponent = dynamic(() => import("./components/UnixTerminal"), {
+    ssr: false
+})
 
 export default function Home() {
   return (
     <main className="h-screen w-full">
-      <UnixTerminal />
+      <TerminalComponent />
     </main>
   );
 }
